f291987f143ba1c18f8cc25c5ea91a24e18b2467
Ryan <hoducthinh2006@gmail.com>
2026-09-14 18:01:37 +0700

    Tao bang Giao Vien

--- tables/Database-project.sql
+++ tables/Database-project.sql
@@ -1,1 +1,13 @@
-create database DatabaseProject
+-- create database DatabaseProject;
+use DatabaseProject;
+
+create table Giang_Vien(
+	MaGV int primary key,
+    HoTen varchar(50) not null,
+    Email varchar(50) not null,
+    TrinhDo varchar(50) not null,
+    ChuyenMon varchar(50) not null,
+    CCCD varchar(50) not null,
+    unique(Email, CCCD)
+);
+
